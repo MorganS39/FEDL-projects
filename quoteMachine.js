aced6130c99646c6b944f94abcee71f43b90760f
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-02-17 10:29:48 -1000

    trying again

--- quoteMachine.js
+++ quoteMachine.js
@@ -48,10 +48,8 @@ var aurthorArray = [
 function newQuote() {
     var randomQuote = Math.floor(Math.random() * (quoteArray.length));
     document.getElementById("text").innerHTML = quoteArray[randomQuote];
-};
-
-// function to put new author in when button is clicked
-function newAuthor() {
     var randomAuthor = Math.floor(Math.random() * (authorArray.length));
     document.getElementById("author").innerHTML = authorArray[randomAuthor];
 };
+
+// function to put new author in when button is clicked
