2948dd6299d098feb1157ef790dee69aa881e65b
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-02-17 09:30:08 -1000

    added author array

--- quoteMachine.js
+++ quoteMachine.js
@@ -15,23 +15,41 @@ user story#10: ability to tweet current quote by clicking #tweet-quote``a elemen
 user story#11: the #quote-box wrapper element should be horizontally centered.
             run a test with browser's zoom at 100% and page maximized
 */
-var currentQuote = "";
-var currentAuthor = "";
 
+// array of random quotes i found
 var quoteArray = [
-    "And makes us rather bear those ills we have Than fly to others that we know not of\? - William Shakespeare",
-    "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment - Ralph Waldo Emerson",
-    "For every minute you are angry you lose sixty seconds of happiness. - Ralph Waldo Emerson",
-    "Finish each day and be done with it. You have done what you could. Some blunders and absurdities no doubt crept in; forget them as soon as you can. Tomorrow is a new day. You shall begin it serenely and with too high a spirit to be encumbered with your old nonsense. - Ralph Waldo Emerson",
-    "It is one of the blessings of old friends that you can afford to be stupid with them. - Ralph Waldo Emerson",
-    "What lies behind us and what lies before us are tiny matters compared to what lies within us. - Ralph Waldo Emerson",
-    "Politics is not a bad profession. If you succeed there are many rewards, if you disgrace yourself you can always write a book. - Ronald Reagan",
-    "Good company and good discourse are the very sinews of virtue. - Izaak Walton",
-    "Behind every great fortune there is a crime. - Honore de Balzac",
-    "We've heard that a million monkeys at a million keyboards could produce the complete works of Shakespeare; now, thanks to the Internet, we know that is not true. - Robert Welensky"
+    "And makes us rather bear those ills we have Than fly to others that we know not of\?",
+    "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment",
+    "For every minute you are angry you lose sixty seconds of happiness.",
+    "Finish each day and be done with it. You have done what you could. Some blunders and absurdities no doubt crept in; forget them as soon as you can. Tomorrow is a new day. You shall begin it serenely and with too high a spirit to be encumbered with your old nonsense.",
+    "It is one of the blessings of old friends that you can afford to be stupid with them.",
+    "What lies behind us and what lies before us are tiny matters compared to what lies within us.",
+    "Politics is not a bad profession. If you succeed there are many rewards, if you disgrace yourself you can always write a book.",
+    "Good company and good discourse are the very sinews of virtue.",
+    "Behind every great fortune there is a crime.",
+    "I wear the chain I forged in life.  I made it link by link, and yard by yard; I girded it on my own free will, and of my own free will I wore it."
+    "We've heard that a million monkeys at a million keyboards could produce the complete works of Shakespeare; now, thanks to the Internet, we know that is not true."
 ];
 
+// array of random authors
+var aurthorArray = [
+    "William Shakespeare",
+    "Ralph Waldo Emerson",
+    "Izaak Walton",
+    "Honore de Balzac",
+    "Ronald Reagan",
+    "Robert Welensky",
+    "Jacob Marley"
+];
+
+// function to put new quote in when button is clicked
 function newQuote() {
     var randomQuote = Math.floor(Math.random() * (quoteArray.length));
     document.getElementById("text").innerHTML = quoteArray[randomQuote];
+}
+
+// function to put new author in when button is clicked
+function newAuthor() {
+    var randomAuthor = Math.floor(Math.random()* authorArray.length));
+    document.getElementById("author").innerHTML = authorAarry[randomAuthor];
 }
