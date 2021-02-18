47cc1414fabee1eb475a04dd9188854c0cc8100a
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-02-18 12:03:39 -1000

    fixed formatting errors in app.js

--- markdown/src/App.js
+++ markdown/src/App.js
@@ -2,16 +2,43 @@ import React from 'react';
 import Badge from "react-bootstrap/Badge";
 
 export default class App extends React.Component{
-render(){
+  
+render() {
   return (
-    <div className="App">
-      <div className="container">
-        <h1>
-          <Badge className="text-align-center" variant="light">
-          Markdown Previewer
-          </Badge>
-      </h1>
+      <div className="App">
+        <div className="container">
+          <div className="row mt-4">
+            <div className="col text-center">
+              <h1>
+                <Badge className="text-align-center" variant="light">
+                  Markdown Previewer
+                </Badge>
+              </h1>
+            </div>
+          </div>
+          <div className="row mt-4">
+            <div className="col-md-6">
+              <div className="col text-center">
+                <h4>
+                  <Badge className="text-align-center" variant="secondary">
+                    Markdown Input
+                  </Badge>
+                </h4>
+              </div>
+            </div>
+
+            <div className="col-md-6">
+              <div className="col text-center">
+                <h4>
+                  <Badge className="text-align-center" variant="secondary">
+                    Preview
+                  </Badge>
+                </h4>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  );}
+    );
+  }
 }
