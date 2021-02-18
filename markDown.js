07497c5d480d01a5853ebdcae602fb40b7925fb0
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-02-18 10:55:01 -1000

    updated JS and linked css wtih basic style

--- markDown.js
+++ markDown.js
@@ -1,13 +1,20 @@
 function render() {
-    var input = document.getElementById("editor");
-    var preview = `
-<h1>This is an h1 header</h1>
-<h2>This is an h2 subheader</h2>
-<a href="#">This is a link</a>
-<p>Here is an example of JavaScript code: 
-      function newQuote() {
-    var randomQuote = Math.floor(Math.random() * (quoteArray.length));
-    document.getElementById("text").innerHTML = quoteArray[randomQuote];
-};
-`
-};
+    var inp     = document.getElementById("editor");
+	var data = `
+<svg xmlns="http://www.w3.org/2000/svg" width="${inp.offsetWidth}" height="${inp.offsetHeight}">
+<foreignObject width="100%" height="100%">
+<div xmlns="http://www.w3.org/1999/xhtml" 
+style="font-family:monospace;font-style: normal; font-variant: normal; font-size:13.3px;padding:2px;;">
+${inp.value} <i style="color:red">cant touch this</i>
+</div>
+</foreignObject>
+</svg>`;
+	var blob = new Blob( [data], {type:'image/svg+xml'} );
+	var url=URL.createObjectURL(blob);
+	inp.style.backgroundImage="url("+URL.createObjectURL(blob)+")";
+ }
+ onload=function(){
+  render();
+  ro = new ResizeObserver(render);
+	ro.observe(document.getElementById("box"));
+ };
