29d419fbd69a41edbb9b61680854ec9ad32674d8
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-02-22 06:51:41 -1000

    added Q-C button framework

--- drum/src/App.js
+++ drum/src/App.js
@@ -21,6 +21,80 @@ render() {
                   <h1>
                     Sample text
                   </h1>
+                  <div className="drum-pad">
+                  <a 
+                        class="button"
+                        id="Q"
+                        title="Q audio"
+                        src="#"
+                    >
+                      Q
+                    </a>
+                    <a 
+                        class="button"
+                        id="W"
+                        title="W audio"
+                        src="#"
+                    >
+                      W
+                    </a>
+                    <a 
+                        class="button"
+                        id="E"
+                        title="E audio"
+                        src="#"
+                    >
+                      E
+                    </a>
+                    <a 
+                        class="button"
+                        id="A"
+                        title="A audio"
+                        src="#"
+                    >
+                      A
+                    </a>
+                    <a 
+                        class="button"
+                        id="S"
+                        title="S audio"
+                        src="#"
+                    >
+                      S
+                    </a>
+                    <a 
+                        class="button"
+                        id="D"
+                        title="D audio"
+                        src="#"
+                    >
+                      D
+                    </a>
+                    <a 
+                        class="button"
+                        id="Z"
+                        title="Z audio"
+                        src="#"
+                    >
+                      Z
+                    </a>
+                    <a 
+                        class="button"
+                        id="X"
+                        title="X audio"
+                        src="#"
+                    >
+                      X
+                    </a>
+                    <a 
+                        class="button"
+                        id="C"
+                        title="C audio"
+                        src="#"
+                    >
+                      C
+                    </a>
+                  </div>
                 </div>
               </div>
             </div>
