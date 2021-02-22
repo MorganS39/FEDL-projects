248fa9ad98f41842bf67790ddaa9164aee8d388f
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-02-22 06:41:40 -1000

    added style containers

--- drum/src/App.js
+++ drum/src/App.js
@@ -15,7 +15,14 @@ render() {
       <div id="drum-machine">
         <div className="App">
           <div className="container">
-            
+            <div className="row mt-4">
+              <div className="col text-center">
+                <h1>
+                  Sample text
+                </h1>
+
+              </div>
+            </div>
           </div>
         </div>
       </div>  
