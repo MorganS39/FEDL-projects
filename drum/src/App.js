414e3df8700eb22a342984ab8b7d5ab422362a25
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-02-22 06:18:38 -1000

    added drum-machine div to contain other elements

--- drum/src/App.js
+++ drum/src/App.js
@@ -2,21 +2,23 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div id="drum-machine">
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </div>  
   );
 }
 
