88fb162732e7ef148718d8edc25e4672a86f3745
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-02-24 16:14:26 -1000

    working on playing audio onClick

--- drum/src/App.js
+++ drum/src/App.js
@@ -25,53 +25,53 @@ render() {
                     <div className="drum-pad">
                       <button id="qAudio">
                         Q
-                        <audio class="clip" id="Q" src="https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3">Q</audio>
+                        <audio class="clip" id="Q" src="https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3"></audio>
                       </button>
                     </div>
                     <div className="drum-pad">
                       <button id="wAudio">
                         W
-                        <audio class="clip" id="W" src="https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3">W</audio>
+                        <audio class="clip" id="W" src="https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3"></audio>
                       </button>
                     </div>
                     <div className="drum-pad">
                       <button id="eAudio">
                         E
-                        <audio class="clip" id="E" src="https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3">E</audio>
+                        <audio class="clip" id="E" src="https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3"></audio>
                       </button>
                     </div>
                     <div className="drum-pad">
                       <button id="aAudio">
                         A
-                        <audio class="clip" id="A" src="https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3">A</audio>
+                        <audio class="clip" id="A" src="https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3"></audio>
                       </button>
                     </div> <div className="drum-pad">
                       <button id="sAudio">
                         S
-                        <audio class="clip" id="S" src="https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3">S</audio>
+                        <audio class="clip" id="S" src="https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3"></audio>
                       </button>
                     </div> <div className="drum-pad">
                       <button id="dAudio">
                         D
-                        <audio class="clip" id="D" src="https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3">D</audio>
+                        <audio class="clip" id="D" src="https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3"></audio>
                       </button>
                     </div>
                     </div> <div className="drum-pad">
                       <button id="zAudio">
                         Z
-                        <audio class="clip" id="Z" src="https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3">Z</audio>
+                        <audio class="clip" id="Z" src="https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3"></audio>
                       </button>
                     </div>
                     </div> <div className="drum-pad">
                       <button id="xAudio">
                         X
-                        <audio class="clip" id="X" src="https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3">X</audio>
+                        <audio class="clip" id="X" src="https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3"></audio>
                       </button>
                     </div>
                     <div className="drum-pad">
-                      <button id="cAudio">
+                      <button id="cAudio" onClick="play">
                         C
-                        <audio class="clip" id="C" src="https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3">C</audio>
+                        <audio class="clip" id="C" src="https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3"></audio>
                       </button>
                     </div>
                 </div>
