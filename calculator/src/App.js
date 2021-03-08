ce310124cb3f01e6619be5f21ce1712bab9b5bc1
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-03-08 12:04:27 -1000

    changed divs for each item to buttons

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -10,63 +10,31 @@ export default class App extends React.Component {
 
   render () {
     // This will show 0 on start and enter numbers on the display as they're clicked
-    let calcface = "0"
     if (this.state.numbPress) {
-      calcface = ${padNumber[this.state.numbPress]};
     }
   
     return (
       <div className="container">
-        <div className="keypad">
+        <div className="keypad" col="3" row="3">
           {/* Basic structure of numberpad 0-9 and = sign */}
           <div className="numberPad">
-            <div id="equals">
-              <a>=</a>
-            </div>
-            <div id="zero">
-              <a>0</a>
-            </div>
-            <div id="one">
-              <a>1</a>
-            </div>
-            <div id="two">
-              <a>2</a>
-            </div>
-            <div id="three">
-              <a>3</a>
-            </div>
-            <div id="four">
-              <a>4</a>
-            </div>
-            <div id="five">
-              <a>5</a>
-            </div>
-            <div id="six">
-              <a>6</a>
-            </div>
-            <div id="seven">
-              <a>7</a>
-            </div>
-            <div id="eight">
-              <a>8</a>
-            </div>
-            <div id="nine">
-              <a>9</a>
-            </div>
+            <button id="equals">=</button>
+            <button id="zero">0</button>
+            <button id="one">1</button>
+            <button id="two">2</button>
+            <button id="three">3</button>
+            <button id="four">4</button>
+            <button id="five">5</button>
+            <button id="six">6</button>
+            <button id="seven">7</button>
+            <button id="eight">8</button>
+            <button id="nine">9</button>
           </div>
           <div className="mathOperators">
-            <div id="add">
-              <a>+</a>
-            </div>
-            <div id="subtract">
-              <a>-</a>
-            </div>
-            <div id="multiply"> 
-              <a>*</a>
-            </div>
-            <div id="divide">
-              <a>/</a>
-            </div>
+            <button id="add">+</button>
+            <button id="subtract">-</button>
+            <button id="multiply">*</button>
+            <button id="divide">/</button>
           </div>
         </div>
       </div>
