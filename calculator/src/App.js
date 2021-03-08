6aba909988e8af4973bedd4984a8ee4b5763042a
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-03-08 12:14:34 -1000

    added two bootstrap style divs

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -15,27 +15,31 @@ export default class App extends React.Component {
   
     return (
       <div className="container">
-        <div className="keypad" col="3" row="3">
-          {/* Basic structure of numberpad 0-9 and = sign */}
-          <div className="numberPad">
-            <button id="equals">=</button>
-            <button id="zero">0</button>
-            <button id="one">1</button>
-            <button id="two">2</button>
-            <button id="three">3</button>
-            <button id="four">4</button>
-            <button id="five">5</button>
-            <button id="six">6</button>
-            <button id="seven">7</button>
-            <button id="eight">8</button>
-            <button id="nine">9</button>
-          </div>
-          {/* Basic structure of the operators for math */}
-          <div className="mathOperators">
-            <button id="add">+</button>
-            <button id="subtract">-</button>
-            <button id="multiply">*</button>
-            <button id="divide">/</button>
+        <div className="row mt-4">
+          <div className="col text-center">
+            <div className="keypad">
+              {/* Basic structure of numberpad 0-9 and = sign */}
+              <div className="numberPad">
+                <button id="equals">=</button>
+                <button id="zero">0</button>
+                <button id="one">1</button>
+                <button id="two">2</button>
+                <button id="three">3</button>
+                <button id="four">4</button>
+                <button id="five">5</button>
+                <button id="six">6</button>
+                <button id="seven">7</button>
+                <button id="eight">8</button>
+                <button id="nine">9</button>
+              </div>
+              {/* Basic structure of the operators for math */}
+              <div className="mathOperators">
+                <button id="add">+</button>
+                <button id="subtract">-</button>
+                <button id="multiply">*</button>
+                <button id="divide">/</button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
